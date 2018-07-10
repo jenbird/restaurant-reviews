86fd97316992664da36eacbc4edc4e7bd3341ea7
jennifersmithuk <jennifersmithuk@gmail.com>
2018-07-10 12:32:48 +0100

    feat: HTML lang and aria role/labels

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: 'sk.eyJ1IjoiamVubmlmZXJzbWl0aHVrIiwiYSI6ImNqajhwNXNzdjE0YW0zcHBsOHl6NHh1a3cifQ.OsJ1G9yVxxvz-0xe1FJVqA>',
+    mapboxToken: 'sk.eyJ1IjoiamVubmlmZXJzbWl0aHVrIiwiYSI6ImNqajhwNXNzdjE0YW0zcHBsOHl6NHh1a3cifQ.OsJ1G9yVxxvz-0xe1FJVqA',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
